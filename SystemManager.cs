1961a7c507890dc46195417c63b9ff55eb15f380
TomeHane <dover1274@gmail.com>
2021-12-12 20:56:54 +0900

    [Update] SystemManager.cs

--- SystemManager.cs
+++ SystemManager.cs
@@ -19,9 +19,31 @@ public class SystemManager : MonoBehaviour
     //Wallオブジェクトをアタッチ
     public GameObject WallObject;
 
+
+    //部屋の高さの最小・最大値
+    [SerializeField]
+    const int roomMinHeight = 5;
+    [SerializeField]
+    const int roomMaxHeight = 10;
+
+    //部屋の横幅の最小・最大値
+    [SerializeField]
+    const int roomMinWidth = 5;
+    [SerializeField]
+    const int roomMaxWidth = 10;
+
+    //部屋数の最小・最大値
+    [SerializeField]
+    const int RoomCountMin = 10;
+    [SerializeField]
+    const int RoomCountMax = 15;
+
     void Start()
     {
         ResetMapData();
+
+        CreateSpaceData();
+
         CreateDangeon();
     }
 
@@ -40,6 +62,67 @@ public class SystemManager : MonoBehaviour
         }
     }
 
+
+    private void CreateSpaceData()
+    {
+        //roomCount(部屋の数)をランダムで決める
+        int roomCount = Random.Range(RoomCountMin, RoomCountMax);
+
+        //部屋数分、処理を行う
+        for (int i = 0; i < roomCount; i++)
+        {
+            //部屋の高さ、横幅をランダムで決める
+            int roomHeight = Random.Range(roomMinHeight, roomMaxHeight);
+            int roomWidth = Random.Range(roomMinWidth, roomMaxWidth);
+
+            //部屋の作成地点(左下)を決める
+            //壁の端から必ず2m離れるように調整
+            //マップサイズが50 * 50の場合、[1～50, 1～50]を調整した値になる
+            int roomPointX = Random.Range(2, MapWidth - roomMaxWidth - 2);
+            int roomPointY = Random.Range(2, MapWidth - roomMaxWidth - 2);
+
+            //道の開始地点を、部屋のグリッドの範囲からランダムに決める
+            int roadStartPointX = Random.Range(roomPointX, roomPointX + roomWidth);
+            int roadStartPointY = Random.Range(roomPointY, roomPointY + roomHeight);
+
+            //これまでの情報をもとに、部屋を作成する
+            //既に作った部屋と重なった場合、trueが返ってくる
+            bool isRoad = CreateRoomData(roomHeight, roomWidth, roomPointX, roomPointY);
+        }
+    }
+
+
+    // 部屋データを生成。すでに部屋がある場合はtrueを返し、道を作らないようにする
+    /// <param name="roomHeight">部屋の高さ</param>
+    /// <param name="roomWidth">部屋の横幅</param>
+    /// <param name="roomPointX">部屋の始点(x)</param>
+    /// <param name="roomPointY">部屋の始点(y)</param>
+    /// <returns></returns>
+    private bool CreateRoomData(int roomHeight, int roomWidth, int roomPointX, int roomPointY)
+    {
+        bool beCreateRoad = false;
+
+        for (int i = 0; i < roomHeight; i++)
+        {
+            for (int j = 0; j < roomWidth; j++)
+            {
+                //該当マスが既に"road"だった場合
+                if (Map[roomPointY + i, roomPointX + j] == road)
+                {
+                    //戻り値をtrueにする
+                    beCreateRoad = true;
+                }
+                else
+                {
+                    //該当マスを"road"にする
+                    Map[roomPointY + i, roomPointX + j] = road;
+                }
+            }
+        }
+        return beCreateRoad;
+    }
+
+
     // マップデータをもとにダンジョンを生成
     private void CreateDangeon()
     {
@@ -49,7 +132,7 @@ public class SystemManager : MonoBehaviour
             {
                 if (Map[i, j] == wall)
                 {
-                    //マップサイズが50 * 50の場合、(±25, ±25, 0)の座標にWallオブジェクトを生成している
+                    //マップサイズが50 * 50の場合、(-25～24, -25～24, 0)の座標にWallオブジェクトを生成している
                     //j, iが1ずつ変化していくので、Wallも1mずつ配置されていく。
                     Instantiate(WallObject, new Vector3(j - MapWidth / 2, i - MapHeight / 2, 0), Quaternion.identity);
                 }
