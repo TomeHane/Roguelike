cc79bef6516b3afb8919b6748d16e3bd0120fc19
TomeHane <dover1274@gmail.com>
2021-12-14 19:48:19 +0900

    [Update] SystemManager.cs

--- SystemManager.cs
+++ SystemManager.cs
@@ -1,3 +1,9 @@
+/*
+ *  【基本的なマップ生成の流れ】
+ *  ①グリッドで壁オブジェクトを作るところ・作らないところを決めていく
+ *  ②壁オブジェクトを生成
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -38,6 +44,11 @@ public class SystemManager : MonoBehaviour
     [SerializeField]
     const int RoomCountMax = 15;
 
+
+    //道の集合点を増やしたいならこれを増やす
+    [SerializeField]
+    const int meetPointCount = 1;
+
     void Start()
     {
         ResetMapData();
@@ -63,10 +74,29 @@ public class SystemManager : MonoBehaviour
     }
 
 
+    //Wallオブジェクトを配置しない箇所を決定する
     private void CreateSpaceData()
     {
         //roomCount(部屋の数)をランダムで決める
         int roomCount = Random.Range(RoomCountMin, RoomCountMax);
+
+
+        //meetPointCount(道の集合点の数)ぶん要素を持つint配列を用意する
+        int[] meetPointsX = new int[meetPointCount];
+        int[] meetPointsY = new int[meetPointCount];
+
+        //meetPointCount(道の集合点の数)ぶん、for文を回す
+        for (int i = 0; i < meetPointCount; i++)
+        {
+            //道の集合点の座標をランダムで決める
+            //範囲はマップ全体を16分割したときの真ん中４エリア
+            meetPointsX[i] = Random.Range(MapWidth / 4, MapWidth * 3 / 4);
+            meetPointsY[i] = Random.Range(MapHeight / 4, MapHeight * 3 / 4);
+
+            //道の集合点は当然"road"マス
+            Map[meetPointsY[i], meetPointsX[i]] = road;
+        }
+
 
         //部屋数分、処理を行う
         for (int i = 0; i < roomCount; i++)
@@ -88,6 +118,15 @@ public class SystemManager : MonoBehaviour
             //これまでの情報をもとに、部屋を作成する
             //既に作った部屋と重なった場合、trueが返ってくる
             bool isRoad = CreateRoomData(roomHeight, roomWidth, roomPointX, roomPointY);
+
+
+            //部屋が"road"と重なっていない場合
+            if (isRoad == false)
+            {
+                //道を作成する
+                //meetPointは乱数で決める(0以上meetPointCount未満)
+                CreateRoadData(roadStartPointX, roadStartPointY, meetPointsX[Random.Range(0, meetPointCount)], meetPointsY[Random.Range(0, meetPointCount)]);
+            }
         }
     }
 
@@ -109,17 +148,131 @@ public class SystemManager : MonoBehaviour
                 //該当マスが既に"road"だった場合
                 if (Map[roomPointY + i, roomPointX + j] == road)
                 {
-                    //戻り値をtrueにする
+                    //部屋が重なってる場合、もうCreateRoadData()は動かさなくてよい
+                    //→isRoadをtrueにする
                     beCreateRoad = true;
                 }
                 else
                 {
                     //該当マスを"road"にする
+                    //∵部屋は全て"road"でもある(マスは全てwallかroad)
                     Map[roomPointY + i, roomPointX + j] = road;
                 }
             }
         }
         return beCreateRoad;
+    }
+
+
+    // 道データを生成
+    /// <param name="roadStartPointX"></param>
+    /// <param name="roadStartPointY"></param>
+    /// <param name="meetPointX"></param>
+    /// <param name="meetPointY"></param>
+    private void CreateRoadData(int roadStartPointX, int roadStartPointY, int meetPointX, int meetPointY)
+    {
+        //道の始点が、集合点よりも右か？
+        bool isRight;
+        if (roadStartPointX > meetPointX)
+        {
+            isRight = true;
+        }
+        else
+        {
+            isRight = false;
+        }
+
+        //道の始点が、集合点よりも下か？
+        bool isUnder;
+        if (roadStartPointY > meetPointY)
+        {
+            isUnder = false;
+        }
+        else
+        {
+            isUnder = true;
+        }
+
+        //二分の一の確率で、道をどの方向から作るかを決める
+        //X方向から道を作る場合
+        if (Random.Range(0, 2) == 0)
+        {
+            //道の始点と集合点のX座標が等しくなければ
+            while (roadStartPointX != meetPointX)
+            {
+                //"現在の"道の始点をroadマスにする
+                Map[roadStartPointY, roadStartPointX] = road;
+
+                //道の始点が集合点よりも右にある場合
+                if (isRight == true)
+                {
+                    //道のX座標を-1する
+                    roadStartPointX--;
+                }
+                //道の始点が集合点よりも左にある場合
+                else
+                {
+                    //道のX座標を+1する
+                    roadStartPointX++;
+                }
+
+            }
+
+            //道の始点と集合点のY座標が等しくなければ
+            while (roadStartPointY != meetPointY)
+            {
+                //"現在の"道の始点をroadマスにする
+                Map[roadStartPointY, roadStartPointX] = road;
+
+                //道の始点が集合点よりも下にある場合
+                if (isUnder == true)
+                {
+                    //道のY座標を+1する
+                    roadStartPointY++;
+                }
+                //道の始点が集合点よりも上にある場合
+                else
+                {
+                    //道のY座標を-1する
+                    roadStartPointY--;
+                }
+            }
+
+        }
+        //Y方向から道を作る場合
+        else
+        {
+
+            while (roadStartPointY != meetPointY)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isUnder == true)
+                {
+                    roadStartPointY++;
+                }
+                else
+                {
+                    roadStartPointY--;
+                }
+            }
+
+            while (roadStartPointX != meetPointX)
+            {
+
+                Map[roadStartPointY, roadStartPointX] = road;
+                if (isRight == true)
+                {
+                    roadStartPointX--;
+                }
+                else
+                {
+                    roadStartPointX++;
+                }
+
+            }
+
+        }
     }
 
 
