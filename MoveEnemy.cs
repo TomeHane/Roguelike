027dd4a468ab163afecc18a6d733faceaff99285
TomeHane <dover1274@gmail.com>
2021-12-27 03:33:47 +0900

    [Update] MoveEnemy.cs
    [Add] SearchArea

--- MoveEnemy.cs
+++ MoveEnemy.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
-	//自オブジェクト
-	Animator animator;
+	//自オブジェクトから取得
 	Rigidbody rb;
+	//SearchArea.csで使用
+	[System.NonSerialized]
+	public Animator animator;
+
+	//他オブジェクトからタグで検索して取得
+	GameObject player;
 	//スポーン可能エリアを確認して目的地を設定する
 	SystemManager systemManager;
 
 	//最新のモンスターの座標
 	Vector3 latestPos;
 	//目的地
-	Vector3 destination;
+	Vector3 _destination;
 	//移動方向
 	Vector3 direction;
+	//移動速度
+	Vector3 velocity;
 
 	//全体の速度倍率
 	[SerializeField]
@@ -30,24 +37,41 @@ public class MoveEnemy : MonoBehaviour
 	//"歩く"ときの速度比率
 	[SerializeField]
 	float walkSpeed = 1.0f;
-	//"走る"ときの速度比率(未確定)
+	//"走る"ときの速度比率
 	[SerializeField]
-	float runSpeed = 2.0f;
-
-	//移動速度
-	Vector3 velocity;
+	float runSpeed = 4.0f;
 
 	//animatorのパラメータ名
 	const string PARAMETER_MOVESPEED = "moveSpeed";
 	const string PARAMETER_IS_OVERLOOKING = "isOverlooking";
 
 	//アニメーションクリップ"Overlooking"の再生時間
-	float clipLengthOverlooking = 0f;
+	float clipLengthOverlooking;
 
-	//到着フラグ
-	bool isArrived = false;
+	//攻撃レンジ(m)
+	[SerializeField]
+	float attackRange = 2.0f;
+	//攻撃時の静止時間(秒)
+	[SerializeField]
+	float attackTime = 8.0f;
+
 	//"Update()で一度だけ動く処理"を実現するためのフラグ
 	bool isOnceCalled = false;
+	//(目的地)到着フラグ
+	bool isArrived = false;
+	//攻撃中フラグ
+	bool isAttacking = false;
+
+	//モンスターの状態。Commonが通常時で、Chaseが追跡時を表す。
+	public enum Status
+    {
+		Common,
+		Chase
+	}
+
+	//SearchArea.csで使用
+	[System.NonSerialized]
+	public Status status;
 
 
 	void Start()
@@ -57,6 +81,7 @@ public class MoveEnemy : MonoBehaviour
 		rb = GetComponent<Rigidbody>();
 
 		//他オブジェクトから取得
+		player = GameObject.FindGameObjectWithTag("Player");
 		systemManager = GameObject.FindGameObjectWithTag("SystemManager").GetComponent<SystemManager>();
 
 		//Overlookingの再生時間を取得する
@@ -69,6 +94,9 @@ public class MoveEnemy : MonoBehaviour
 				clipLengthOverlooking = clip.length;
 			}
 		}
+
+		//ステータスを"通常時"で初期化
+		status = Status.Common;
 	}
 
 	void Update()
@@ -82,49 +110,34 @@ public class MoveEnemy : MonoBehaviour
 			isOnceCalled = true;
 		}
 
+        //通常時なら
+        if (status == Status.Common)
+        {
+			CommonMove();
+		}
+        //追跡時なら
+        if (status == Status.Chase)
+        {
+			ChaseMove();
+        }
+		
+	}
+
+
+	//通常時（プレイヤーを見つけていないとき）の処理
+	void CommonMove()
+	{
 		//目的地に到着していないなら
 		if (!isArrived)
 		{
 			//向きを変える処理
-			//進んでいる方向をベクトルで取得
-			Vector3 diff = transform.position - latestPos;
-			//前回のpositionを更新
-			latestPos = transform.position;
+			ChangeRotation();
 
-			//ベクトルの大きさが0.01以上なら
-            if (diff.magnitude > 0.01f)
-            {
-				//進んでいる方向に向くための角度を取得
-				Quaternion rot = Quaternion.LookRotation(diff);
-				//現在の角度と目標角度とで、球面線形補間
-				rot = Quaternion.Slerp(transform.rotation, rot, 0.2f);
-				//向きを変える
-				transform.rotation = rot;
-            }
+			//目的地と使用するアニメーションから、移動方向と移動速度を決める
+			DecideVelocity(_destination, walkSpeed);
 
-			//Vector3 targetPos = destination;
-			//y座標を自分と同じにすることで二次元に制限する
-			//targetPos.y = transform.position.y;
-			//transform.LookAt(targetPos);
-
-
-			//移動方向を決める
-			//移動方向(矢印) = 目的地 - 現在地
-			direction = destination - transform.position;
-			//y方向への移動を0にしておく
-			direction.y = 0f;
-			//ベクトルの長さが1になるように調節
-			direction.Normalize();
-			//移動速度を決める
-			velocity = direction * speedMagnification;
-
-			//Animatorのパラメータ値もセットする
-			moveSpeed = walkSpeed;
-			animator.SetFloat(PARAMETER_MOVESPEED, moveSpeed);
-
-
-			//　目的地に到着したかどうかの判定
-			if (Vector3.Distance(transform.position, destination) < 5.0f)
+			//目的地に到着した場合
+			if (Vector3.Distance(transform.position, _destination) < 5.0f)
 			{
 				//到着フラグを立てる
 				isArrived = true;
@@ -142,11 +155,88 @@ public class MoveEnemy : MonoBehaviour
 	}
 
 
-    private void FixedUpdate()
+	//追跡時（プレイヤーを見つけたとき）の処理
+	void ChaseMove()
+    {
+        //攻撃フラグがオフ（攻撃範囲外）なら
+        if (!isAttacking)
+        {
+			//向きを変える処理
+			ChangeRotation();
+
+			//目的地と使用するアニメーションから、移動方向と移動速度を決める
+			DecideVelocity(player.transform.position, runSpeed);
+
+            //攻撃範囲に入った場合
+            if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+            {
+				//攻撃フラグを立てる
+				isAttacking = true;
+
+				//プレイヤーの方を向く
+				transform.LookAt(player.transform);
+
+				//攻撃アニメーションを再生する
+				animator.SetTrigger("attack");
+				//動きを止める
+				velocity = Vector3.zero;
+				moveSpeed = standSpeed;
+				animator.SetFloat(PARAMETER_MOVESPEED, moveSpeed);
+
+				//一定時間後に攻撃フラグを解除するコルーチンを飛ばす
+				StartCoroutine(StandStillAttacking());
+			}
+		}
+    }
+
+
+	private void FixedUpdate()
     {
 		//移動させる
 		rb.velocity = velocity;
     }
+
+
+	//向きを変える処理
+	void ChangeRotation()
+    {
+		//進んでいる方向をベクトルで取得
+		Vector3 diff = transform.position - latestPos;
+		//前回のpositionを更新
+		latestPos = transform.position;
+
+		//ベクトルの大きさが0.01以上なら
+		if (diff.magnitude > 0.01f)
+		{
+			//進んでいる方向に向くための角度を取得
+			Quaternion rot = Quaternion.LookRotation(diff);
+			//現在の角度と目標角度とで、球面線形補間
+			rot = Quaternion.Slerp(transform.rotation, rot, 0.2f);
+			//向きを変える
+			transform.rotation = rot;
+		}
+	}
+
+
+	//(移動方向と)移動速度を決める処理
+	void DecideVelocity(Vector3 destination, float anySpeed)
+    {
+		//移動方向を決める
+		//移動方向(矢印) = 目的地 - 現在地
+		direction = destination - transform.position;
+		//y方向への移動を0にしておく
+		direction.y = 0f;
+		//ベクトルの長さが1になるように調節
+		direction.Normalize();
+
+		//moveSpeedを決める
+		moveSpeed = anySpeed;
+		//Animatorのパラメータ値もセットする
+		animator.SetFloat(PARAMETER_MOVESPEED, moveSpeed);
+
+		//移動速度を決める
+		velocity = direction * speedMagnification * moveSpeed;
+	}
 
 
 	//目的地を決める関数
@@ -160,10 +250,10 @@ public class MoveEnemy : MonoBehaviour
 			int x = systemManager.spawnablePointListX[randomNum];
 			int y = systemManager.spawnablePointListY[randomNum];
 
-			destination = new Vector3(systemManager.squareLength * (x - systemManager.MapWidth / 2), 0, systemManager.squareLength * (y - systemManager.MapHeight / 2));
+			_destination = new Vector3(systemManager.squareLength * (x - systemManager.MapWidth / 2), 0, systemManager.squareLength * (y - systemManager.MapHeight / 2));
 
 			//半径40m以内なら、目的地を確定
-			if (Vector3.Distance(transform.position, destination) < 40.0f)
+			if (Vector3.Distance(transform.position, _destination) < 40.0f)
 			{
 				break;
 			}
@@ -186,5 +276,13 @@ public class MoveEnemy : MonoBehaviour
 		DecideDestination();
 		//到着フラグを切る（移動再開）
 		isArrived = false;
+    }
+
+
+	//攻撃時、静止する時間
+	IEnumerator StandStillAttacking()
+    {
+		yield return new WaitForSeconds(attackTime);
+		isAttacking = false;
     }
 }
---
+++ SearchArea.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//SearchAreaトリガーはPlayerレイヤーにしか反応しない
+public class SearchArea : MonoBehaviour
+{
+    //親オブジェクトのスクリプトをアタッチ
+    [SerializeField]
+    MoveEnemy moveEnemy;
+    //追跡時間
+    [SerializeField]
+    float chaseTime = 10.0f;
+
+    //プレイヤーが視界に入っているかの判定
+    bool isInSight = false;
+
+
+    //Playerが視界に入ったら
+    private void OnTriggerEnter(Collider other)
+    {
+        isInSight = true;
+
+        //モンスターのステータスが通常時の場合
+        if (moveEnemy.status == MoveEnemy.Status.Common)
+        {
+            //モンスターのステータスを"追跡時"に変更する
+            moveEnemy.status = MoveEnemy.Status.Chase;
+            //"Overlooking"を解除しておく
+            moveEnemy.animator.SetBool("isOverlooking", false);
+            //一定時間後にステータスの判定を行うコルーチンを動かす
+            StartCoroutine(CheckStatus());
+        }
+    }
+
+
+    //Playerが視界から外れたら
+    private void OnTriggerExit(Collider other)
+    {
+        isInSight = false;
+    }
+
+
+    //ステータスを変更するかどうかを判定する
+    IEnumerator CheckStatus()
+    {
+        yield return new WaitForSeconds(chaseTime);
+
+        //視界内なら
+        if (isInSight)
+        {
+            //自身のコルーチンをもう一度飛ばす
+            StartCoroutine(CheckStatus());
+        }
+        //視界外なら
+        else
+        {
+            //ステータスを"通常時"に戻す
+            moveEnemy.status = MoveEnemy.Status.Common;
+        }
+    }
+}
